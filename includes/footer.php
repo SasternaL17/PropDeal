1f7aa8a1a56fc533ba407c690dcaca4c61ea7e02
SASWATA DHARA <109872994+SasternaL17@users.noreply.github.com>
2023-02-14 11:10:39 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Kolkata">Kolkata</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2023 Copyright PropDeal </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
